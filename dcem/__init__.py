13cd92699992d4c77ccc2553f51afb336014bc75
Brandon Amos <brandon.amos.cs@gmail.com>
2020-07-21 10:03:50 -0400

    Add copyright headers

--- dcem/__init__.py
+++ dcem/__init__.py
@@ -1,2 +1,4 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
 from .dcem import dcem
 from .dcem_ctrl import dcem_ctrl
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
 from setuptools import find_packages, setup
 
 setup(
